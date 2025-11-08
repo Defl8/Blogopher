5e480d06d82530d66376447fc1000fe05a499174
Wyatt James <wyattsamueljames@gmail.com>
2025-11-08 16:48:39 -0700

    feat: add basic post making endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,10 +11,15 @@ const Port string = ":42069"
 
 func main() {
 	router := gin.Default()
+	
+	router.Static("/static", "./web/static/")
+	router.LoadHTMLGlob("web/templates/*")
 
 
-	router.GET("/test", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "dan smells")
+	router.GET("/make", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "post.html", gin.H{})
 	})
+
+
 	router.Run(Port)
 }
